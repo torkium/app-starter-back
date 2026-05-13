2fc572cdc64d31f34fc42c4fe36a45096b5ae1c4
torkium <julien.taront@gmail.com>
2026-05-13 09:21:56 +0000

    Add Symfony web profiler

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
